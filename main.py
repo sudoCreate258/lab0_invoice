d4ad9fe389e9233f565cadf136c7ca5726771b96
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-02-02 11:45:22 -0500

    Fix string quotes and update tax rate variable

---
+++ main.py
@@ -0,0 +1,13 @@
+from helper import *
+# DO NOT UPDATE CODE ABOVE THIS LINE
+
+#TODO: (Input) Assign data to the variables.
+#TODO: (Process) Calculate the total amount.
+#TODO: (Output) Display invoice with information on the sale. 
+
+"""
+Ready for the extra credit?
+
+TAX_RATE = get_tax_rate(False)
+print(x)
+"""
