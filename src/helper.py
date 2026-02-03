23cf31bafdb0cf436d40545ff9eb2712fdf17936
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-02-03 15:06:23 -0500

    refactored help() and reduced repetition

--- src/helper.py
+++ src/helper.py
@@ -4,35 +4,30 @@ import random as ran
 
 tax_dct = {
     "national":(10,37),
-    "state":(0,13.3),
-    "local":(0,3.8)
+    "state"   :(0,13.3),
+    "local"   :(0,3.8)
 }
 
-def help(dct, key, indx=0):
-    t = dct[key][indx]
-    return int(t)
+def help(key):
+    t = tax_dct[key]
+    return f"{ran.randint(int(t[0]), int(t[1]))}.{ran.randint(0,99)}"
 
 def get_tax_rate(flag=True):
     if flag:
         tax_type = ran.randint(0,2)
         tax = 0.0
-        
+       
         for i,e in enumerate(tax_dct):
             if i == tax_type:
-                tax = ran.randint(help(tax_dct,e), help(tax_dct,e,1) ) 
-                return f"{tax}.{ran.randint(0,99)}"
+                return help(e)
     else:
-
-            
-        nat_tax = ran.randint( help(tax_dct,"national"), help(tax_dct,"national",1) )
-        ste_tax = ran.randint( help(tax_dct,"state"),    help(tax_dct,"state",1)    )
-        loc_tax = ran.randint( help(tax_dct,"local"),    help(tax_dct,"local",1)    )
-        
-        zipLst = zip([nat_tax,ste_tax,loc_tax], tax_dct.keys())
-        
-        tax_lst = [f"{a} tax:{b}.{ran.randint(0,99)}" for (b,a) in zipLst]
+        nat_tax = help("national")
+        ste_tax = help("state")
+        loc_tax = help("local")
+       
+        zipLst = zip(tax_dct.keys(),[nat_tax,ste_tax,loc_tax])
+        tax_lst = [f"{a} tax:{b}" for (a,b) in zipLst]
         return ",".join(tax_lst)
-        
 
 def get_cost():
     val = ['500', '5000', '50']
